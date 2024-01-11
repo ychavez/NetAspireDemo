c83ff626dc5754da3a5bf0764319dc9ef4dcd6d6
Yael Chavez <you400@gmail.com>
2024-01-11 13:56:25 -0600

    aspire demo

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,6 +6,7 @@ namespace Backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.AddServiceDefaults();
 
             // Add services to the container.
 
@@ -15,6 +16,8 @@ namespace Backend
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.MapDefaultEndpoints();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
